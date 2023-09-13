bda93832a90449d457db6918bad23bf289ad7f79
prasitzkp <prasit562@gmail.com>
2023-09-13 17:10:33 +0700

    Add Function - Random Vocab from API(Mock)

--- scripts.js
+++ scripts.js
@@ -1,3 +1,62 @@
+
+
+// let progress = 0;
+
+// function increaseProgress() {
+//   if (progress < 10) {
+//     progress++;
+//     updateProgressBar();
+//   }
+// }
+
+// function updateProgressBar() {
+//   let elem = document.getElementById("myBar");
+//   let width = (progress / 10) * 100;
+//   elem.style.width = width + "%";
+//   elem.innerHTML = progress + " / 10";
+// }
+
+// function changeColor(button, color) {
+//   if (color === "greenBtn") {
+//     button.classList.toggle("greenBtn");
+//   } else if (color === "redBtn") {
+//     button.classList.toggle("redBtn");
+//   }
+// }
+
+
+
+//@ 1. ดึงข้อมูลมาจาก Database
+//@ 2. นำข้อมูลจาก Database มาสุ่ม ก่อนแสดงผล
+//@ 3. นำ คำแปลไทย(vocabTH) สุ่มลงในปุ่ม Choice A,B
+//@ 4. ถ้าคำตอบที่ถูกต้องสุ่มได้ A คำตอบที่ผิดจะเป็น B
+//@ 5. เมื่อเลือกคำตอบ ถ้าตอบถูก คะแนนจะ +1 / ผิด = 0
+//@ 6. ไม่ว่าจะตอบ ถูก หรือ ผิด ก็ไปยังข้อถัดไป
+
+// เรียกใช้ฟังก์ชันสลับ
+//shuffleArray(data.vocab);
+
+// vocabTextDOM.innerText = data.vocab[0].vocabEN;
+// partsOfDOM.innerText = `(${data.vocab[0].partOf})`;
+// btnChoiceA.innerText = data.vocab[0].vocabTH;
+// ฟังก์ชันสลับตำแหน่งของออบเจ็กต์ในอาร์เรย์
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    // สุ่มตำแหน่งในอาร์เรย์
+    let j = Math.floor(Math.random() * (i + 1));
+
+    // สลับตำแหน่งของออบเจ็กต์
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  return array;
+}
+
+let point = 0;
+
 const data = {
   vocab: [
     { vocabEN: "Cat", vocabTH: "แมว", partOf: "n." },
@@ -12,57 +71,26 @@ const data = {
     { vocabEN: "Fish", vocabTH: "ปลา", partOf: "n." },
   ],
 };
+console.log("START RANDOM");
+  console.log('API DATA : ',data);
 
-let progress = 0;
-
-function increaseProgress() {
-  if (progress < 10) {
-    progress++;
-    updateProgressBar();
-  }
+window.onload = () =>{
+  showQuestion();
 }
 
-function updateProgressBar() {
-  let elem = document.getElementById("myBar");
-  let width = (progress / 10) * 100;
-  elem.style.width = width + "%";
-  elem.innerHTML = progress + " / 10";
-}
 
-function changeColor(button, color) {
-  if (color === "greenBtn") {
-    button.classList.toggle("greenBtn");
-  } else if (color === "redBtn") {
-    button.classList.toggle("redBtn");
-  }
-}
 
-// ฟังก์ชันสลับตำแหน่งของออบเจ็กต์ในอาร์เรย์
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        // สุ่มตำแหน่งในอาร์เรย์
-        let j = Math.floor(Math.random() * (i + 1));
-        
-        // สลับตำแหน่งของออบเจ็กต์
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
+const showQuestion = async () => {
+  const vocabTextDOM = document.getElementById("vocab-text");
+  const partsOfDOM = document.getElementById("vocab-parts");
+  const btnChoiceA = document.getElementById("btn-choice1");
+  const btnChoiceB = document.getElementById("btn-choice2");
 
-// เรียกใช้ฟังก์ชันสลับ
-shuffleArray(data.vocab);
+  
 
-// แสดงผลลัพธ์หลังจากสลับตำแหน่ง
-console.log(data);
+  const vocabAll = data.vocab; //? เปรียบเหมือน ใช้ Axios ดึงมา
+  console.log("Variable VocabAll : ", vocabAll);
 
-console.log(data.vocab[0].vocabTH);
-
-const vocabTextDOM = document.getElementById('vocab-text')
-const partsOfDOM = document.getElementById('vocab-parts')
-const btnChoiceA = document.getElementById('btn-choice1')
-const btnChoiceB = document.getElementById('btn-choice2')
-
-vocabTextDOM.innerText = data.vocab[0].vocabEN;
-partsOfDOM.innerText = `(${data.vocab[0].partOf})`;
-btnChoiceA.innerText = data.vocab[0].vocabTH;
+  const vocabRandom = await shuffleArray([...vocabAll]); //? สุ่มข้อมูลที่ได้มาจาก API ใหม่ทั้งหมดเพื่อให้การแสดงผลแต่ละครั้ง จะได้ไม่เหมือนกัน
+  console.log("VocabRandom Line : ", vocabRandom[0].vocabEN);
+};
